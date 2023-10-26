061e7f0be6756ae217b18523abde727e6a309c18
laiiyuu0308 <laiiyuu0308@gmail.com>
2023-10-26 08:51:15 +0000

    list

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import List from './compoents/List.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="100" height="100" />
 
     <div class="wrapper">
       <HelloWorld msg="TodoList" />
---
+++ src/components/List.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class='todo-list'>
+    <div>
+        <input v-model="todo" @keyup.enter="add" type="text">
+        <button @click="add" >新增</button>
+    </div>
+    <ul class="list-group">
+        <li v-for="item in items" :key="item">
+            <span>{{item}}</span>
+
+            <button @click="remove(item)">
+                <span>&times;</span>
+            </button>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script steup>
+  //todo function
+   const toDo = () =>{
+   const todo = Vue.ref('');
+   const items = Vue.ref(['Vue','is','Awesome']);   
+  }
+  //新增功能
+  const add = () => {
+      if (todo.value) {
+          items.value.push(todo.value);
+          todo.value ='';
+      };
+  };
+  //移除功能
+  const remove = item => {
+      item.value = items.value.filter( v => v !== item);
+  }
+  return{
+      toDo,
+      items,
+      add,
+      remove
+  };
+
+</script>
+
+<style scoped>
+
+</style>
